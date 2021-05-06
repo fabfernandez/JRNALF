b17f4b3203c13f50c571f250f9cde9c659e1ae60
juan-aguirre-ml <aguirre.juan@mercadolibre.com>
2021-05-06 16:35:30 -0300

    Added DTOs for "parts/orders/{orderNumberCM}" endpoint.

---
+++ src/main/java/com/mercadolibre/finalchallengedemo/dtos/orderstatus/OrderDetailsDTO.java
@@ -0,0 +1,28 @@
+package com.mercadolibre.finalchallengedemo.dtos.orderstatus;
+
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+@Validated
+public class OrderDetailsDTO {
+
+    private Integer partCode;
+    private String description;
+
+    @Min(value = 0, message = "Quantity must be a positive integer")
+    private Integer quantity;
+
+    @Pattern(regexp = "^[RGrg]$", message = "Invalid order status code. ")
+    private String accountType; //Warranty or Spare Parts account type
+
+    @Size(max = 100)
+    private String reason;     //Why is the order pending
+
+    @Pattern(regexp = "^[NDnd]$", message = "Invalid order status code. ")
+    private String partStatus; //Availability N = Normal, D = Delayed
+}
---
+++ src/main/java/com/mercadolibre/finalchallengedemo/dtos/orderstatus/OrderStatusResponseDTO.java
@@ -0,0 +1,27 @@
+package com.mercadolibre.finalchallengedemo.dtos.orderstatus;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Data
+@Validated
+public class OrderStatusResponseDTO {
+
+    @Pattern(regexp = "^\\d{4}-\\d{8}$", message = "Order number does not match the required pattern.")
+    private String orderNumberCE;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate orderDate;
+
+    @Size(min = 1, max = 1, message = "Order status code must be only one letter.")
+    @Pattern(regexp = "^[PDFCpdfc]$", message = "Invalid order status code. ")
+    private String orderStatus;
+
+    private ArrayList<OrderDetailsDTO> orderDetails;
+}
