fd76faa8bb241289df55afe885f3c396756b6871
Lucas Martin Klassen <lucas.klassen@mercadolibre.com>
2021-05-07 18:20:47 -0300

    Testing part service

--- src/test/java/com/mercadolibre/finalchallengedemo/unit/service/PartServiceTest.java
+++ src/test/java/com/mercadolibre/finalchallengedemo/unit/service/PartServiceTest.java
@@ -5,8 +5,11 @@ import com.mercadolibre.finalchallengedemo.dtos.PartDTO;
 import com.mercadolibre.finalchallengedemo.dtos.PartFilterDTO;
 import com.mercadolibre.finalchallengedemo.dtos.response.PartResponseDTO;
 import com.mercadolibre.finalchallengedemo.entities.PartEntity;
+import com.mercadolibre.finalchallengedemo.entities.StockSubsidiaryEntity;
+import com.mercadolibre.finalchallengedemo.exceptions.PartsNotFoundException;
 import com.mercadolibre.finalchallengedemo.repository.IPartRepository;
 import com.mercadolibre.finalchallengedemo.repository.IStockRepository;
+import com.mercadolibre.finalchallengedemo.security.DecodeToken;
 import com.mercadolibre.finalchallengedemo.service.PartServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,9 +19,9 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.util.ResourceUtils;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import javax.servlet.http.Part;
+import java.time.Instant;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,6 +33,7 @@ public class PartServiceTest {
     private IPartRepository partRepository;
     @Mock
     private IStockRepository stockRepository;
+
     private PartServiceImpl partService;
 
     private Date pastDate;
@@ -51,20 +55,26 @@ public class PartServiceTest {
         List<PartEntity> partsEntityList = getList("classpath:allPartsEntities.json",PartEntity.class);
         List<PartDTO> partsDtoList = getList("classpath:allParts.json",PartDTO.class);
 
+        StockSubsidiaryEntity stock = new StockSubsidiaryEntity();
+
+        when(this.stockRepository.findStockByPartCodeAndSubsidiary(any(),any())).thenReturn(stock);
         when(this.partRepository.findAll()).thenReturn(partsEntityList);
 
         final PartResponseDTO response = partService.getAll();
 
-        assertEquals(partsDtoList.size(), response.getParts().size());
+        assertIterableEquals(response.getParts(), partsDtoList);
         verify(partRepository,times(1)).findAll();
     }
 
     @Test
     @DisplayName("Given parts, then return filtered parts")
     public void givenParts_thenReturnFilteredParts() {
-        List<PartEntity> partsEntityList = getList("classpath:allPartsEntities.json",PartEntity.class);
-        List<PartDTO> partsDtoList = getList("classpath:allParts.json",PartDTO.class);
+        List<PartEntity> partsEntityList = getList("classpath:filteredPartsEntities.json",PartEntity.class);
+        List<PartDTO> partsDtoList = getList("classpath:filteredParts.json",PartDTO.class);
 
+        StockSubsidiaryEntity stock = new StockSubsidiaryEntity();
+
+        when(this.stockRepository.findStockByPartCodeAndSubsidiary(any(),any())).thenReturn(stock);
         when(this.partRepository.findPartsModifiedSinceDate(any(),any())).thenReturn(partsEntityList);
 
         PartFilterDTO validFilter = new PartFilterDTO();
@@ -73,8 +83,50 @@ public class PartServiceTest {
 
         final PartResponseDTO response = partService.getPartsByFilter(validFilter);
 
-        assertEquals(partsDtoList.size(), response.getParts().size());
+        assertIterableEquals(response.getParts(), partsDtoList);
         verify(partRepository,times(1)).findPartsModifiedSinceDate(any(),any());
+    }
+
+
+    @Test
+    @DisplayName("No parts found, then throw NoPartsFoundException")
+    public void noPartsFound_thenThrowNoPartsFoundException() {
+
+        StockSubsidiaryEntity stock = new StockSubsidiaryEntity();
+
+        when(this.stockRepository.findStockByPartCodeAndSubsidiary(any(),any())).thenReturn(stock);
+        when(this.partRepository.findPartsWithPriceModifiedSinceDate(any(),any())).thenReturn(new ArrayList<PartEntity>());
+
+        PartFilterDTO filter = new PartFilterDTO();
+        filter.setQueryType('V');
+        filter.setDate(Date.from(Instant.now()));
+
+        assertThrows(PartsNotFoundException.class, () -> partService.getPartsByFilter(filter));
+        verify(partRepository,times(1)).findPartsWithPriceModifiedSinceDate(any(),any());
+    }
+
+    @Test
+    @DisplayName("Given part, when find, then return part")
+    public void givenPart_whenFind_thenReturnPart() {
+        PartDTO partDTO = new PartDTO(1,"test",null,"A",1,1,1,1,1,1,"maker",pastDate,pastDate);
+        PartEntity partEntity = new PartEntity(1,"test",1,1,1,1,1,1,pastDate,pastDate,"maker","A",null);
+        Optional<PartEntity> part = Optional.of(partEntity);
+
+        when(this.partRepository.findById(any())).thenReturn(part);
+
+        assertEquals(partDTO,this.partService.findPart(1));
+
+        verify(partRepository,times(1)).findById(any());
+    }
+
+    @Test
+    @DisplayName("Given part, when find, then return part")
+    public void whenFindByNonExistentId_thenThrowNotFoundException() {
+        when(this.partRepository.findById(any())).thenReturn(Optional.ofNullable(null));
+
+        assertThrows(PartsNotFoundException.class,() -> this.partService.findPart(113));
+
+        verify(partRepository,times(1)).findById(any());
     }
 
 
