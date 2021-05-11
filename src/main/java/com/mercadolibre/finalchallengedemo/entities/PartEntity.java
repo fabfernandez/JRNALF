7b1ccc374f0eb0923ca898ba8437d2f535877ca0
sogliano <nicolas.sogliano@mercadolibre.com>
2021-05-11 13:04:25 -0300

    part entity update

--- src/main/java/com/mercadolibre/finalchallengedemo/entities/PartEntity.java
+++ src/main/java/com/mercadolibre/finalchallengedemo/entities/PartEntity.java
@@ -3,6 +3,7 @@ package com.mercadolibre.finalchallengedemo.entities;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
@@ -12,34 +13,34 @@ import java.util.Set;
 @Entity
 @Table(name = "parts")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PartEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "part_code")
-    @Size(min = 8, message = "The field needs a minimum of 8 numeric characters")
+    //@Size(min = 8, message = "The field needs a minimum of 8 numeric characters")
     private Integer partCode;
 
     @Column(nullable = false, length = 100)
-    @Size(max = 100, message = "The field needs a miximium of 100 alphanumeric characters.")
+    @Size(max = 100, message = "The field needs a maximium of 100 alphanumeric characters.")
     private String description;
 
     @Column(nullable = false, length = 4)
-    @Size(max = 4, message = "The width of the spare part needs 4 numeric characters.")
+    //@Size(max = 4, message = "The width of the spare part needs 4 numeric characters.")
     private Integer widthDimension;
 
     @Column(nullable = false, length = 4)
-    @Size(max = 4, message = "The height of the spare part needs 4 numeric characters.")
+    //@Size(max = 4, message = "The height of the spare part needs 4 numeric characters.")
     private Integer tallDimension;
 
     @Column(nullable = false, length = 4)
-    @Size(max = 4, message = "The length of the spare part needs 4 numeric characters.")
+    //@Size(max = 4, message = "The length of the spare part needs 4 numeric characters.")
     private Integer longDimension;
 
     @Column(nullable = false, length = 5)
-    @Size(max = 5, message = "The net weight of the spare part needs 5 numeric characters.")
+    //@Size(max = 5, message = "The net weight of the spare part needs 5 numeric characters.")
     private Integer netWeight;
 
     @Column(name = "normal_price")
@@ -63,7 +64,14 @@ public class PartEntity {
     @OneToMany(mappedBy = "part")
     private Set<StockSubsidiaryEntity> stockSubsidiaryEntities;
 
-    //
-    @OneToMany(mappedBy = "")
-    private Set<DealerOrderItems> orderItemEntities;
+    //@OneToMany(mappedBy = "")
+    //private Set<OrderItemEntity> orderItemEntities;
+
+    /*
+    @ManyToMany
+    @JoinTable(name = "subsidiaries_stock",
+            joinColumns = @JoinColumn(name = "part_code"),
+            inverseJoinColumns = @JoinColumn(name = "id_subsidiary"))
+    private Set<SubsidiaryEntity> subsidiaries;
+     */
 }
