59eeb14b2ce944ae5ce030929d134b5ef9446e41
juan-aguirre-ml <aguirre.juan@mercadolibre.com>
2021-05-11 16:58:33 -0300

    Modified PartEntity order of parameters to be equal to the partDto

--- src/main/java/com/mercadolibre/finalchallengedemo/entities/PartEntity.java
+++ src/main/java/com/mercadolibre/finalchallengedemo/entities/PartEntity.java
@@ -27,6 +27,12 @@ public class PartEntity {
     @Size(max = 100, message = "The field needs a maximium of 100 alphanumeric characters.")
     private String description;
 
+    @Column(name ="maker")
+    private String maker;
+
+    @Column(name ="discount_type")
+    private String discountType;
+
     @Column(nullable = false, length = 4)
     //@Size(max = 4, message = "The width of the spare part needs 4 numeric characters.")
     private Integer widthDimension;
@@ -55,23 +61,8 @@ public class PartEntity {
     @Column(name ="last_price_modification")
     private Date lastPriceModification;
 
-    @Column(name ="maker")
-    private String maker;
-
-    @Column(name ="discount_type")
-    private String discountType;
 
     @OneToMany(mappedBy = "part")
     private Set<StockSubsidiaryEntity> stockSubsidiaryEntities;
 
-    //@OneToMany(mappedBy = "")
-    //private Set<OrderItemEntity> orderItemEntities;
-
-    /*
-    @ManyToMany
-    @JoinTable(name = "subsidiaries_stock",
-            joinColumns = @JoinColumn(name = "part_code"),
-            inverseJoinColumns = @JoinColumn(name = "id_subsidiary"))
-    private Set<SubsidiaryEntity> subsidiaries;
-     */
 }
